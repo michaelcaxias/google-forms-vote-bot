c8b61bac0480a7a2842aa0b35bb331b98f74c987
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-23 19:44:03 -0300

    init: adiciona arquivos iniciais

---
+++ index.js
@@ -0,0 +1,50 @@
+import fetch from "node-fetch";
+import fs from "fs";
+
+const luizBody = {
+  form_id: "626472606fddfb4a1398b066",
+  email: "",
+  responses: [
+    {
+      question_id: "626472606fddfb4a1398b068",
+      type: "MULTIPLE_CHOICE",
+      choice: "626472606fddfb4a1398b06a",
+    },
+  ],
+  submit_time: "1650752926000",
+  storage_used: 0,
+};
+
+const increaseVotes = () => {
+  const votes = fs.readFileSync("votes.txt", "utf8");
+  const newVotes = parseInt(votes) + 1;
+  fs.writeFileSync("votes.txt", newVotes.toString());
+  console.log(`Voto computado: ${newVotes}`);
+};
+
+const vote = async (body) => {
+  try {
+    const request = await fetch("https://surveyheart.com/response", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      body: JSON.stringify(body),
+    });
+    const response = await request.json();
+    if (response.status === 200) {
+      increaseVotes();
+    }
+  } catch (error) {
+    console.error(error);  
+  }
+};
+
+const main = async () => {
+  for (let index = 0; index < 100; index += 1) {
+    await vote(luizBody);
+  }
+};
+
+main();
