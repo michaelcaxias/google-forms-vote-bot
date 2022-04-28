9d5da360639ee94e87c9f1a23ed4c0b7aaca6c90
Michael <michaelcaxias.dev@gmail.com>
2022-04-27 23:45:41 -0300

    fix: ajusta trechos quebrados

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import * as puppeteer from 'puppeteer';
 import * as fs from 'fs';
-import { NOME, LINK } from './link-e-nome';
 
 const increaseVotes = () => {
   const votes = fs.readFileSync('numero-de-votos.txt', 'utf8');
@@ -10,17 +9,22 @@ const increaseVotes = () => {
 }
 
 const navigationPage = async (browser: puppeteer.Browser) => {
-  const URL = LINK;
+  // aqui será o link do formulário (tem que ser entre aspas)
+  const URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfyphK58J62aLt-ZKMnKN0ktA5-0arIB6thvjFnKJFCYExF9Q/viewform';
+
   const page = await browser.newPage();
   console.log('Navigating to page...');
   await page.goto(URL);
-
+  
   console.log('Waiting for navigation...');
   await page.waitForSelector('.ulDsOb');
-
+  
   await page.$$eval('.ulDsOb', (options) => { 
     for (const option of options) {
-      if (option.textContent === NOME) {
+      // aqui será o texto da opção que queremos votar (tem que ser entre aspas)
+      const NAME = 'B'
+
+      if (option.textContent === NAME) {
         option.className = 'pessoa-votada';
       } else {
         option.className = 'pessoa-nao-votada';
@@ -31,21 +35,21 @@ const navigationPage = async (browser: puppeteer.Browser) => {
   console.log('Voting...');
   await page.click('.pessoa-votada');
 
-  await page.screenshot({ path: '/imagens/vote.png' });
+  await page.screenshot({ path: './imagens/vote.png' });
 
   console.log('Submitting...');
   await page.click('.QvWxOd');
 
   await page.waitForNavigation();
   increaseVotes()
-  await page.screenshot({ path: '/imagens/sumitted.png' });
+  await page.screenshot({ path: './imagens/sumitted.png' });
   await page.close();
   console.log('Page closed');
 }
 
 const main = async () => {
   const browser: puppeteer.Browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
 
   for (let index = 0; index < 1000; index += 1) { 
--- link-e-nome.ts
+++
@@ -1,2 +0,0 @@
-export const NOME = 'nome da pessoa' // o nome terá que ser igualzinho o que está no formulário
-export const LINK = 'link do formulário' // o link do formulário 
