b94877dfc5b47e87799c1a79c91da01799301bbd
Michael <michaelcaxias.dev@gmail.com>
2022-05-07 22:09:12 -0300

    :sparkles: feat: adiciona readlinesync

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as puppeteer from 'puppeteer';
 import * as fs from 'fs';
+import * as readlineSync from 'readline-sync';
 
 const increaseVotes = () => {
   const votes = fs.readFileSync('numero-de-votos.txt', 'utf8');
@@ -8,9 +9,23 @@ const increaseVotes = () => {
   fs.writeFileSync('numero-de-votos.txt', newVotes.toString());
 }
 
-const navigationPage = async (browser: puppeteer.Browser) => {
+const addClassToOptions = async (optionText: string, page: puppeteer.Page) => {
+  await page.$$eval('.ulDsOb', (options, optionSelected) => { 
+    for (const option of options) {
+      // aqui será o texto da opção que queremos votar (tem que ser entre aspas)
+      const NAME = optionSelected;
+  
+      if (option.textContent === NAME) {
+        option.className = 'pessoa-votada';
+      } else {
+        option.className = 'pessoa-nao-votada';
+      }
+    }
+  }, optionText);
+}
+
+const navigationPage = async (browser: puppeteer.Browser, URL: string, option: string) => {
   // aqui será o link do formulário (tem que ser entre aspas)
-  const URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfyphK58J62aLt-ZKMnKN0ktA5-0arIB6thvjFnKJFCYExF9Q/viewform';
 
   const page = await browser.newPage();
   console.log('Navigating to page...');
@@ -19,18 +34,7 @@ const navigationPage = async (browser: puppeteer.Browser) => {
   console.log('Waiting for navigation...');
   await page.waitForSelector('.ulDsOb');
   
-  await page.$$eval('.ulDsOb', (options) => { 
-    for (const option of options) {
-      // aqui será o texto da opção que queremos votar (tem que ser entre aspas)
-      const NAME = 'B'
-
-      if (option.textContent === NAME) {
-        option.className = 'pessoa-votada';
-      } else {
-        option.className = 'pessoa-nao-votada';
-      }
-    }
-  });
+  await addClassToOptions(option, page);
 
   console.log('Voting...');
   await page.click('.pessoa-votada');
@@ -48,12 +52,17 @@ const navigationPage = async (browser: puppeteer.Browser) => {
 }
 
 const main = async () => {
+  const URL = readlineSync.question('Link do formulário: ');
+  const option = readlineSync.question('Nome da pessoa: ');
+  const openBrowser = readlineSync.question('Abrir navegador? (s/n): ');
+  const transformAnswerToBool = openBrowser !== 's';
+
   const browser: puppeteer.Browser = await puppeteer.launch({
-    headless: true,
+    headless: transformAnswerToBool,
   });
 
   for (let index = 0; index < 1000; index += 1) { 
-    await navigationPage(browser);
+    await navigationPage(browser, URL, option);
   }
 
   await browser.close();
