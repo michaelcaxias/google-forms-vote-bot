467939054b4a57214271f8f4404957d27edfb1da
Michael <michaelcaxias.dev@gmail.com>
2022-05-07 22:56:20 -0300

    feat: utiliza cores no terminal

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import * as puppeteer from 'puppeteer';
 import * as fs from 'fs';
 import * as readlineSync from 'readline-sync';
+import * as colors from 'colors';
 
 const increaseVotes = () => {
   const votes = fs.readFileSync('numero-de-votos.txt', 'utf8');
   const newVotes = parseInt(votes) + 1;
-  console.log(`==================== Voto: ${newVotes} ====================`);
+  console.log(colors.rainbow('====================') + colors.white(` Voto: ${newVotes} `) + colors.rainbow('===================='));
   fs.writeFileSync('numero-de-votos.txt', newVotes.toString());
 }
 
@@ -25,34 +26,41 @@ const addClassToOptions = async (optionText: string, page: puppeteer.Page) => {
 
 const navigationPage = async (browser: puppeteer.Browser, URL: string, option: string) => {
   const page = await browser.newPage();
-  console.log('Abrindo pagina...');
-  await page.goto(URL);
+  try {    
+    console.log(colors.bold.green('Abrindo pagina...'));
+    await page.goto(URL);
+    
+    console.log(colors.bold.green('Esperando a tela carregar...'));
+    await page.waitForSelector('.ulDsOb');
+    
+    await addClassToOptions(option, page);
   
-  console.log('Esperando a tela carregar...');
-  await page.waitForSelector('.ulDsOb');
+    console.log(colors.bold.green(`Votando em "${option}"`));
+    await page.click('.pessoa-votada');
   
-  await addClassToOptions(option, page);
+    await page.screenshot({ path: './imagens/vote.png' });
+  
+    console.log(colors.bold.green('Clicando no botão "Enviar"...'));
+    await page.click('.QvWxOd');
+  
+    await page.waitForNavigation();
+    increaseVotes()
+  
+    await page.screenshot({ path: './imagens/sumitted.png' });
+    await page.close();
 
-  console.log('Votando em: ' + option);
-  await page.click('.pessoa-votada');
-
-  await page.screenshot({ path: './imagens/vote.png' });
-
-  console.log('Clicando no botão "Enviar"...');
-  await page.click('.QvWxOd');
-
-  await page.waitForNavigation();
-  increaseVotes()
-  await page.screenshot({ path: './imagens/sumitted.png' });
-  await page.close();
-  console.log('Fechando página...');
+    console.log(colors.bold.green('Fechando página...'));
+  } catch (error) {
+    console.log(colors.bold.red('Algo deu errado, reiniciando programa...'));
+    await page.close();
+  }
 }
 
 const main = async () => {
-  const URL = readlineSync.question('Link do formulario: ');
-  const option = readlineSync.question('Nome da pessoa: ');
-  const openBrowser = readlineSync.question('Abrir navegador? (s/n): ');
-  const votesNumber = readlineSync.question('Numero de votos: ');
+  const URL = readlineSync.question(colors.bold.yellow('Link do formulario: '));
+  const option = readlineSync.question(colors.bold.yellow('Nome da pessoa: '));
+  const openBrowser = readlineSync.question(colors.bold.yellow('Abrir navegador? (s/n): '));
+  const votesNumber = readlineSync.question(colors.bold.yellow('Numero de votos: '));
   console.log(' ');
 
   const transformAnswerToBool = openBrowser !== 's';
@@ -62,11 +70,7 @@ const main = async () => {
   });
 
   for (let index = 0; index < Number(votesNumber); index += 1) {
-    try {
-      await navigationPage(browser, URL, option);
-    } catch (error) {
-      console.log('Algo deu errado, reiniciando o programa...');
-    }
+    await navigationPage(browser, URL, option);
   }
 
   await browser.close();
